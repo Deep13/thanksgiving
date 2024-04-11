ce240dd79a5a7ce55406ecec0a80552fe660c3c4
deep13 <deep.deepak30@gmail.com>
2024-04-11 15:35:36 +0530

    initial commit

---
+++ src/Constants.js
@@ -0,0 +1,235 @@
+export const day_title = 'Happy Thanksgiving';
+export const banner_key1 = 'ca-app-pub-4717579333914549/5156837573';
+export const banner_key2 = 'ca-app-pub-4717579333914549/3574687816';
+export const banner_key3 = 'ca-app-pub-4717579333914549/7163413184';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/9598004833';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/2977945539';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/9351782191';
+// export const language = ['English', 'Hindi', 'Arabic', 'Urdu'];
+export const language = ['English'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'Enjoy each and every moment of this wonderful day and be grateful to God for all the blessings. Happy Thanksgiving to you and your family, have fun.',
+      },
+      {
+        id: '002',
+        text:
+          'May you enjoy the feast and have lots of fun on this wonderful occasion. May you have a home filled with joy, laughter, freedom, and love. Happy Thanksgiving.',
+      },
+      {
+        id: '003',
+        text:
+          'May the good things of life be yours in abundance not only at Thanksgiving but throughout the coming year.',
+      },
+      {
+        id: '004',
+        text:
+          'Wishing you a deliciously sweet Thanksgiving Day surrounded by the ones you love and the ones you cherish. May you have a grand holiday season!',
+      },
+      {
+        id: '005',
+        text:
+          'This Thanksgiving, I wish that God showers you and your family with peace, love, warmth, and joy. Have a joyous Thanksgiving!',
+      },
+      {
+        id: '006',
+        text:
+          'Roast a turkey and call some friends; with the company of family, you are set to celebrate a happy thanksgiving day. Enjoy!',
+      },
+      {
+        id: '007',
+        text:
+          'Count your blessings one by one; its thanksgiving again! Celebrate and be thankful for every good tide. Happy Thanksgiving.',
+      },
+      {
+        id: '008',
+        text:
+          'May your blessings be multiplied this year and throughout all your life. Happy Thanksgiving wishes to you!',
+      },
+      {
+        id: '009',
+        text:
+          'Wishing you a harvest of blessings, good health and good times. Happy Thanksgiving day!',
+      },
+      {
+        id: '010',
+        text:
+          'May you enjoy this celebration with your family full of laughter and peace. Happy Thanksgiving greetings!',
+      },
+      {
+        id: '011',
+        text:
+          'Thanksgiving wishes from across the miles from our house to yours. May your home be filled with laughter and happiness!',
+      },
+      {
+        id: '012',
+        text:
+          'Hope that you’re surrounded with the love of your dear ones on this festive season. Happy Thanksgiving to you and yours.',
+      },
+    ]
+  }
+  // ,
+  // Hindi: {
+  //   "wishes": [
+  //     {
+  //       "id": "001",
+  //       "text": "हर ख्वाहिश हो मंजूर-ए-खुदा मिले, हर कदम पर रजा-ए-खुदा, फना हो लब्ज-ए-गम यही हैं दुआ, बरसती रहे सदा रहमत-ए-खुदा| ईद मुबारक"
+  //     },
+  //     {
+  //       "id": "002",
+  //       "text": "यह ईद-उल-अधा, अल्लाह का बरकत आपके जीवन में खुशी, शांति और समृद्धि ला सकता है। आपको और आपके परिवार को ईद मुबारक!"
+  //     },
+  //     {
+  //       "id": "003",
+  //       "text": "आज का दिन प्रार्थना करने, अल्लाह को याद करने, प्यार फैलाने और अपने प्रियजनों के साथ मनाने का दिन है। आप को ईद मुबारक!"
+  //     },
+  //     {
+  //       "id": "004",
+  //       "text": "मैं आपके लिए अल्लाह का आशीर्वाद चाहता हूं। ईद मुबारक!"
+  //     },
+  //     {
+  //       "id": "005",
+  //       "text": "मुझे उम्मीद है कि आपका जीवन बिरयानी जितना मसालेदार होगा और सेवइयां जितना मीठा होगा। ईद मुबारक!"
+  //     },
+  //     {
+  //       "id": "006",
+  //       "text": "अल्लाह आपके बलिदान को स्वीकार करे और जीवन में आपकी इच्छाओं को पूरा करे। आपको ईद की बहुत-बहुत शुभकामनाएं!"
+  //     },
+  //     {
+  //       "id": "007",
+  //       "text": "हर खुशी बड़ी या छोटी हो सकती है, आपके दरवाजे पर एक संदेश प्रदान करती है कि ... हर दिन ईद है! ईद मुबारक।"
+  //     },
+  //     {
+  //       "id": "008",
+  //       "text": "ईद मुबारक। अल्लाह आज, कल और लगातार आपके साथ रहे।"
+  //     },
+  //     {
+  //       "id": "009",
+  //       "text": "खुशी के साथ आपके दिन, समृद्धि के साथ आपके सप्ताह, संतुष्टि के साथ आपके महीने, और आपकी आँखें प्यार और शांति के साथ! ईद मुबारक।"
+  //     },
+  //     {
+  //       "id": "010",
+  //       "text": "एक बच्चे की तरह आनंद लें और अल्लाह के लिए भी आभारी रहें क्योंकि उसने सभी मुसलमानों को इतने भव्य दिन के लिए शुभकामनाएं दी हैं। आप को ईद मुबारक!"
+  //     },
+  //     {
+  //       "id": "011",
+  //       "text": "मेरी इच्छा है कि आप इसके हर एक मिनट की सराहना करें। भगवान हमारे उपवासों के साथ-साथ प्रार्थनाओं को भी स्वीकार करें। ईद मुबारक।"
+  //     },
+  //     {
+  //       "id": "012",
+  //       "text": "चिंता में रहने से एक ब्रेक ले लो और यह भी ईद है तो चलो मनाते हैं! हमसे जुड़ने के लिए आपका दर। जीवन बहुत स्वादिष्ट है। ईद मुबारक।"
+  //     }
+  //   ]
+  // }
+  // ,
+  // Arabic: {
+  //   "wishes": [
+  //     {
+  //       "id": "001",
+  //       "text": "صلى الله و لطفك و مع عائلتك دائما! عيد مبارك!"
+  //     },
+  //     {
+  //       "id": "002",
+  //       "text": "عيد الأضحى المبارك الله يجلب السعادة والسلام والازدهار في حياتك. عيد مبارك لك ولعائلتك!"
+  //     },
+  //     {
+  //       "id": "003",
+  //       "text": "اليوم هو يوم الصلاة ، اذكر الله ، انشر الحب ، واحتفل مع أحبائنا. عيد مبارك لك!"
+  //     },
+  //     {
+  //       "id": "004",
+  //       "text": "أتمنى لكم بركات الله. عيد مبارك!"
+  //     },
+  //     {
+  //       "id": "005",
+  //       "text": "أتمنى أن تكون حياتك حارة مثل البرياني و حلوة مثل سيفايان. عيد مبارك!"
+  //     },
+  //     {
+  //       "id": "006",
+  //       "text": "تقبل الله ذبيحتكم ورضي رغباتكم في الحياة. نتمنى لكم عيدا سعيدا جدا!"
+  //     },
+  //     {
+  //       "id": "007",
+  //       "text": "نرجو أن تكون كل متعة كبيرة أو صغيرة ، على عتبة داركم لتوصيل رسالة مفادها ... كل يوم عيد! عيد مبارك."
+  //     },
+  //     {
+  //       "id": "008",
+  //       "text": "عيد مبارك. وفقكم الله اليوم وغدا وبشكل مستمر."
+  //     },
+  //     {
+  //       "id": "009",
+  //       "text": "أيامك مع السعادة ، أسابيعك مع الرخاء ، أشهرك مع الارتياح ، وعينيك بالحب والسلام! عيد مبارك."
+  //     },
+  //     {
+  //       "id": "010",
+  //       "text": "استمتعي كطفل وكوني ممتنة لله لأنه منح جميع المسلمين مثل هذا اليوم الرائع. عيد مبارك لك!"
+  //     },
+  //     {
+  //       "id": "011",
+  //       "text": "أتمنى لكم كل دقيقة. تقبل الله صيامنا وصلواتنا. عيد مبارك."
+  //     },
+  //     {
+  //       "id": "012",
+  //       "text": "خذ استراحة من القلق ، وهذا هو العيد ، لذلك دعونا نحتفل! تقييمك للانضمام إلينا. الحياة لذيذة للغاية. عيد مبارك."
+  //     }
+  //   ]
+  // }
+  // ,
+  // Urdu: {
+  //   "wishes": [
+  //     {
+  //       "id": "001",
+  //       "text": "صلى الله و لطفك و مع عائلتك دائما! عيد مبارك!"
+  //     },
+  //     {
+  //       "id": "002",
+  //       "text": "عيد الأضحى المبارك الله يجلب السعادة والسلام والازدهار في حياتك. عيد مبارك لك ولعائلتك!"
+  //     },
+  //     {
+  //       "id": "003",
+  //       "text": "اليوم هو يوم الصلاة ، اذكر الله ، انشر الحب ، واحتفل مع أحبائنا. عيد مبارك لك!"
+  //     },
+  //     {
+  //       "id": "004",
+  //       "text": "أتمنى لكم بركات الله. عيد مبارك!"
+  //     },
+  //     {
+  //       "id": "005",
+  //       "text": "أتمنى أن تكون حياتك حارة مثل البرياني و حلوة مثل سيفايان. عيد مبارك!"
+  //     },
+  //     {
+  //       "id": "006",
+  //       "text": "تقبل الله ذبيحتكم ورضي رغباتكم في الحياة. نتمنى لكم عيدا سعيدا جدا!"
+  //     },
+  //     {
+  //       "id": "007",
+  //       "text": "نرجو أن تكون كل متعة كبيرة أو صغيرة ، على عتبة داركم لتوصيل رسالة مفادها ... كل يوم عيد! عيد مبارك."
+  //     },
+  //     {
+  //       "id": "008",
+  //       "text": "عيد مبارك. وفقكم الله اليوم وغدا وبشكل مستمر."
+  //     },
+  //     {
+  //       "id": "009",
+  //       "text": "أيامك مع السعادة ، أسابيعك مع الرخاء ، أشهرك مع الارتياح ، وعينيك بالحب والسلام! عيد مبارك."
+  //     },
+  //     {
+  //       "id": "010",
+  //       "text": "استمتعي كطفل وكوني ممتنة لله لأنه منح جميع المسلمين مثل هذا اليوم الرائع. عيد مبارك لك!"
+  //     },
+  //     {
+  //       "id": "011",
+  //       "text": "أتمنى لكم كل دقيقة. تقبل الله صيامنا وصلواتنا. عيد مبارك."
+  //     },
+  //     {
+  //       "id": "012",
+  //       "text": "خذ استراحة من القلق ، وهذا هو العيد ، لذلك دعونا نحتفل! تقييمك للانضمام إلينا. الحياة لذيذة للغاية. عيد مبارك."
+  //     }
+  //   ]
+  // }
+
+};
